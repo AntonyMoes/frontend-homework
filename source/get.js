3bcc509e20ee15dcfa5bd6c8d991db4321a82c5a
AntonyMo <moantonymo@gmail.com>
2019-02-25 16:19:34 +0300

    documenation added

--- source/get.js
+++ source/get.js
@@ -1,5 +1,11 @@
 'use strict';
 
+/**
+ * Returns object's property
+ * @param {object} object - The target object.
+ * @param {string} prop - The name of the property of the object to be returned.
+ * @return {object} The returned property
+ */
 const get = function (object, prop) {
     if (typeof object !== 'object' || typeof prop !== 'string') {
         return null;
