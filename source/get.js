5f4c670e731e9da417b74fdd5d8f501fb8b98717
AntonyMo <moantonymo@gmail.com>
2019-02-25 12:39:03 +0300

    argument check added

--- source/get.js
+++ source/get.js
@@ -1,6 +1,14 @@
 'use strict';
 
 const get = function (object, prop) {
+    if (typeof object !== "object") {
+        throw new TypeError("object expected");
+    }
+
+    if (typeof prop !== "string") {
+        throw new TypeError("string expected");
+    }
+
     let propArr = prop.split('.').filter(str => str !== '');
 
     for (prop of propArr) {
